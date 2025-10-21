4f1832330d05d8229e81dabed4ba08fa8882fb1d
Conner West <conner@keysys.io>
2025-10-21 16:31:05 +0000

    Gosh Dang

--- models/silver/dim_providers.sql
+++ models/silver/dim_providers.sql
@@ -6,54 +6,57 @@
   )
 }}
 
-select
-    -- Primary Keys
-    enrollment_id,
-    npi,
-    ccn,
-    associate_id,
-    cah_or_hospital_ccn,
+with source_data as (
+    select * from {{ ref('stg_hospital_enrollments') }}
+),
 
-    -- Foreign Key
-    provider_type_code,
+final as (
+    select
+        -- Primary Keys
+        enrollment_id,
+        npi,
+        ccn,
+        associate_id,
+        cah_or_hospital_ccn,
 
-    -- Org Details
-    organization_name,
-    doing_business_as_name,
-    organization_type_structure,
-    organization_other_type_text,
-    is_proprietary,
-    incorporation_date,
-    incorporation_state,
-    
-    -- Location Details
-    address_line_1,
-    address_line_2,
-    city,
-    state,
-    zip_code,
-    enrollment_state,
-    practice_location_type,
-    location_other_type_text,
+        -- Foreign Key
+        provider_type_code,
 
-    -- Other Flags
-    is_multiple_npi,
-    is_reh_conversion,
-    reh_conversion_date,
+        -- Org Details
+        organization_name,
+        doing_business_as_name,
+        organization_type_structure,
+        organization_other_type_text,
+        is_proprietary,
+        incorporation_date,
+        incorporation_state,
+        
+        -- Location Details
+        address_line_1,
+        address_line_2,
+        city,
+        state,
+        zip_code,
+        enrollment_state,
+        practice_location_type,
+        location_other_type_text,
 
-    -- Metadata for tracking
-    data_source_id,
-    loaded_at
+        -- Other Flags
+        is_multiple_npi,
+        is_reh_conversion,
+        reh_conversion_date,
 
-from
-    {{ ref('stg_hospital_enrollments') }}
+        -- Metadata for tracking
+        data_source_id,
+        loaded_at
 
+    from
+        source_data
+)
+
+select * from final
+
+-- The incremental logic is moved here, to the very end of the query --
 {% if is_incremental() %}
-
-  -- This creates a variable holding the max timestamp from the table *before* the run
-  {% set max_loaded_at = "(select max(loaded_at) from {{ this }})" %}
-
-  -- This filters the new data to only append rows newer than that timestamp
-  where loaded_at > {{ max_loaded_at }}
-
+  where loaded_at > (select max(loaded_at) from {{ this }})
 {% endif %}
